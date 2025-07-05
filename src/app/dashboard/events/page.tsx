01d1d2fb563befe15e5fefebf34588db10f9d792
Keerthik <keerthikcoorgdu@gmail.com>
2025-07-05 08:52:50 +0000

    Update

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -202,7 +202,7 @@ export default function ManageEventsPage() {
                                     <FormControl>
                                        <div className="relative">
                                           <Upload className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
-                                          <Input type="file" className="pl-10" {...imageRef} />
+                                          <Input type="file" accept="image/*" className="pl-10" {...imageRef} />
                                        </div>
                                     </FormControl>
                                     <FormMessage />
