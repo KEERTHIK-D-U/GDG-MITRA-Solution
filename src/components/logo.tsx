12bc7132aa3219bd73c403ee2424fd739099d702
Keerthik <keerthikcoorgdu@gmail.com>
2025-07-08 07:15:02 +0000

    Update

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 interface LogoProps {
@@ -9,13 +8,25 @@ interface LogoProps {
 
 export function Logo({ className, width = 100, height = 35 }: LogoProps) {
   return (
-    <Image
-      src="/logo.png"
-      alt="Mitra Logo"
+    <svg
       width={width}
       height={height}
-      className={cn(className)}
-      priority
-    />
+      viewBox="0 0 120 40"
+      className={cn('text-foreground', className)}
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+      aria-label="Mitra Logo"
+    >
+      <text
+        x="0"
+        y="30"
+        fontFamily='"Playfair Display", serif'
+        fontSize="32"
+        fontWeight="bold"
+        fill="currentColor"
+      >
+        Mitra
+      </text>
+    </svg>
   );
 }
